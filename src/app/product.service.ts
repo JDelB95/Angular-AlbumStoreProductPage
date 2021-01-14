e6823bc5d7081e5d214a93f2df7a30df8bc13443
John Del Bianco <jdelb35@gmail.com>
2021-01-14 09:45:06 -0500

    Module 2p1

---
+++ src/app/product.service.ts
@@ -0,0 +1,9 @@
+import { HttpClient } from '@angular/common/http';
+//import { Injectable } from '@angular/core';
+
+
+export class ProductService {
+
+  // Inject the HttpClient
+  constructor(private http: HttpClient) { }
+}
